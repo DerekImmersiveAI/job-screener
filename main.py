137d474b0dbf78d9a758c9ed2536b754ac2949ac
DerekImmersiveAI <derektnewman@gmail.com>
2025-04-17 15:48:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,6 @@ def push_to_airtable(job, score, reason):
             "url": job.get("url"),
             "job_posted_time": job.get("job_posted_time"),
             "job_num_applicants": job.get("job_num_applicants"),
-            "job_posted_date": job.get("job_posted_date"),
             "base_salary": job.get("base_salary"),
             "Score": score,
             "Reason": reason,
