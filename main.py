c467ac9bd04ec7c1b3303599ea170e2faade580f
DerekImmersiveAI <derektnewman@gmail.com>
2025-04-17 15:45:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ AIRTABLE_BASE_ID = os.getenv("AIRTABLE_BASE_ID")
 AIRTABLE_TABLE_NAME = os.getenv("AIRTABLE_TABLE_NAME")
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_BUCKET = os.getenv("AWS_BUCKET")
+AWS_BUCKET = os.getenv("AWS_BUCKET_NAME")
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 
